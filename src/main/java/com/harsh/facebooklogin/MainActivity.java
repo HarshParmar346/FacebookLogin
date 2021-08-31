4620d4ceaf6224cbdfa031776ed5aa5157d42b19
Harsh Parmar <89838217+HarshParmar346@users.noreply.github.com>
2021-08-31 21:03:08 +0530

    Add files via upload

---
+++ src/main/java/com/harsh/facebooklogin/MainActivity.java
@@ -0,0 +1,26 @@
+package com.harsh.facebooklogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button reg ;
+        reg = findViewById(R.id.reg);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Activity_register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
